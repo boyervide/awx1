2253696179b2ae7b5a1490dd060c237ba1ac0fbf
boyervide <97649044+boyervide@users.noreply.github.com>
2024-05-06 12:25:04 -0500

    testapp.py

--- testapp.py
+++ testapp.py
@@ -1,3 +1,4 @@
+import streamlit as st
 error = st.alert("This is error alert.", title="Error", type="error", icon="🚨")
 if st.button("Open alert error", key="third_spec_alert_button"):
     error.open()
